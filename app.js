4ce670c94833e1c99c09ae71c817ca70fb1a22b3
jaeguen-kim <gungirl492@gmail.com>
2022-03-09 23:39:35 +0900

    typeof 공부

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// module내 import의 경우 로컬 index.html은 js를 인식하지않아 에러표시됨
 import { test } from './src/functions/element.js'
 let btn = document.getElementsByClassName('btn')
 
@@ -14,6 +15,7 @@ function changeColor(evt) {
             break;
     }
 }
+Array.from(btn);
 console.log(typeof btn)
 btn['red'].addEventListener("click", changeColor)
 btn['blue'].addEventListener("click", changeColor)
