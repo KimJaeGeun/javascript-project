0d5e1aa1f365a22afd6881f45ff0a6918860f3ad
jaeguen-kim <gungirl492@gmail.com>
2022-03-22 23:50:22 +0900

    함수형 컴포넌트작성

--- app.js
+++ app.js
@@ -1,36 +1,3 @@
-// module내 import의 경우 로컬 index.html은 js를 인식하지않아 에러표시됨
-// TODO: value체크 함수 이용 코드 작성
-import { isEmpty } from './src/functions/checkValue.js'
-let btn = document.getElementsByClassName('btn')
+import listRender from './src/components/List.js'
 
-function changeColor(evt) {
-    const el = evt.currentTarget
-
-    if (!isEmpty(el)) {
-        switch (evt.currentTarget.id) {
-            case 'red':
-                el.classList.toggle('red');
-                break;
-            case 'blue':
-                el.classList.toggle('blue');
-                break;
-            case 'grey':
-                el.classList.toggle('grey');
-                break;
-            case 'green':
-                el.classList.toggle('green');
-                break;
-        }
-    }
-}
-
-function testMethod() {
-    let test = [];
-    console.log(Boolean(test.length));
-}
-Array.from(btn);
-btn['red'].addEventListener("click", changeColor)
-btn['blue'].addEventListener("click", changeColor)
-btn['grey'].addEventListener("click", changeColor)
-btn['green'].addEventListener("click", changeColor)
-btn['test'].addEventListener("click", testMethod)
+new listRender(document.querySelector('#list'))
--- src/components/List.js
+++ src/components/List.js
@@ -1,29 +1,79 @@
-const list = document.querySelector('#list');
+// module내 import의 경우 로컬 index.html은 js를 인식하지않아 에러표시됨
+// TODO: value체크 함수 이용 코드 작성
+import { isEmpty } from '../functions/checkValue.js'
+
+// const list = document.querySelector('#list');
 
 let state = {
-    statusList: ['Progress', 'pending', 'complite', 'test', 'bug'],
-    btnColor: ['blue', 'grey', 'green', 'blue', 'red']
+    statusList: [{
+            type: 'Progress',
+            status: false,
+            buttonColor: 'blue'
+        },
+        {
+            type: 'pending',
+            status: false,
+            buttonColor: 'grey'
+        },
+        {
+            type: 'complite',
+            status: false,
+            buttonColor: 'green'
+        },
+        {
+            type: 'test',
+            status: false,
+            buttonColor: 'yellow'
+        },
+        {
+            type: 'bug',
+            status: false,
+            buttonColor: 'red'
+        },
+    ],
+}
+const { statusList } = state;
+
+function listRender(comp) {
+    comp.innerHTML = `
+                        <ul>
+                            ${ statusList.map( status =>  createList(status.type) )}
+                        </ul>
+                    `;
+    let btn = document.getElementsByClassName('btn')
+    Array.from(btn).map(item => item.addEventListener("click", changeStutas));
 }
 
-const render = () => {
-        const { statusList } = state;
-        list.innerHTML = `
-    <ul>
-        ${ statusList.map( (status, index) =>  `
-            <li class="list-item">
-                <button type="button" id="${btnColor[index]}" class="btn">${status}</button>
-            </li>
-        ` ) }
-    </ul>
-  `;
-//   document.querySelector('#append').addEventListener('click', () => {
-//     setState({ items: [ ...items, `item${items.length + 1}` ] })
-//   })
+function createList(value) {
+    return `<li class="list-item" id="${value}">
+                <button type="button" id="${value}" class="btn">${value}</button>
+            </li>`;
 }
 
-// const setState = (newState) => {
-//   state = { ...state, ...newState };
-//   render();
-// }
+function changeStutas(evt) {
+    const el = evt.currentTarget
+    const selectedTarget = evt.currentTarget.id
+    let selectedStatus = (selected) => statusList.filter(item => item.type === selected).status = true;
 
-export { render }
+    if (!isEmpty(el)) {
+        selectedStatus(selectedTarget);
+        switch (selectedTarget) {
+            case 'Progress':
+                el.classList.toggle('blue');
+                break;
+            case 'pending':
+                el.classList.toggle('grey');
+                break;
+            case 'complite':
+                el.classList.toggle('green');
+                break;
+            case 'test':
+                el.classList.toggle('yellow');
+                break;
+            case 'bug':
+                el.classList.toggle('red');
+                break;
+        }
+    }
+}
+export default listRender
