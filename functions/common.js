f79d1746da698d01caa97ff49ad25a37683d54b8
KimJaeGeun <99818364+KimJaeGeun@users.noreply.github.com>
2022-04-04 22:35:20 +0900

    조건, 반복문 공부완료

--- functions/common.js
+++ functions/common.js
@@ -1,11 +1,9 @@
-import {mergeArray} from '../type/array/functions.js'
-
 function consoleLog() {
     // 테스트값을 대입하여 확인
-    let value1 = ['value1', 'value2', 'value3', 'value2'];
-    let value2 = { key: 'value4'};
+    let value1 = ['value1', 'value2', Symbol('val'), 'value2'];
+    let value2 = ['value1', 'value5']
 
-    console.log(value1.find(item => item === 'value2'))
+    console.log( 0 || 'qwer')
 }
 
 export { consoleLog }
--- type/array/MEMO.md
+++ type/array/MEMO.md
@@ -70,6 +70,19 @@ console.log(copyArray)
 - 전개연산자를 이용하여 값을 복사하여 할당
 ```
 
+- 배열내 반복함수
+```
+let testArray = ['value1', 'value2', 'value3']
+
+testArray.forEach(item => console.log(item))
+
+
+출력값: ['value1'], ['value2'], ['value3']
+- 값이 지정되지않은 배열은 열거되지않는다.
+- forEach()의 반환값은 undefined
+- 반복도중 예외발생이외에 중단되는 방도가 없기에 중단이유가 있는 반복의 경우 사용하지않는다.
+```
+
 - 배열내 요소 변경 및 새로운 배열 출력
 ```
 let testArray = ['value1', 'value2', 'value3']
